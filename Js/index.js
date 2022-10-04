e210ced4d61e174e046058c70cb6e10e9851a274
Alunos <aluno.etec@etecEmbu.com.br>
2022-10-03 22:33:31 -0300

    aula 03/10

---
+++ Js/index.js
@@ -0,0 +1,56 @@
+console.log ("hello word");
+
+//maior >
+let n1 = 20;
+let n2 = 14;
+console.log(n1 > n2);
+
+
+const name = "Carolina";
+alert("Seja bem vindo," + name );
+
+
+let age = 21;
+alert( name + "! \nVocê tem " + age + " anos!");
+
+
+
+
+
+
+
+
+
+
+//maior ou igual "> + =" : >= usando fira code (fonte)
+let n3 = 20;
+let n4 = 21; 
+console.log (n3 >= n4);
+
+//  === !== == != > >= < <= && || 
+
+
+let n5 = 11;
+let n6 = 10;
+console.log (n5 < n6);
+
+let n7 = 9;
+let n8 = 12;
+console.log (n7 <= n8);
+
+let n9 = 55;
+let n10 = 60;
+console.log (n9 === n10);
+
+let n11 = 20;
+let n12 = 20;
+consolel.log (n11 !== n12);
+
+let n13 = 20;
+let n14 = 20;
+consolel.log (n13 == n14);
+
+let n15 = 20;
+let n16 = 20;
+consolel.log (n15 != n16);
+
